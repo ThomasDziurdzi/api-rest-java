1ec372ac0edf912c25aa6b203f3218b45e321307
Thomas DZIURDZI <thomasdziurdzi@gmail.com>
2025-09-08 12:18:42 +0200

    initial commit

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello, Admin";
+    }
+}
